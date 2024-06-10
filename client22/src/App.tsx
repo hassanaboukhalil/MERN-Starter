95f3e15f0342a492c82852987e2ecc41bb92e26f
hassan <hassanak509@gmail.com>
2024-06-10 17:40:13 +0300

    some changes

---
+++ client22/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold underline">
+        Hello World!
+      </h1>
+      <button className="btn-disabled">DaisyUI Button</button>
+    </div>
+  )
+}
+
+export default App

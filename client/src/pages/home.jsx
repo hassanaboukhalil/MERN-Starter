217d7282ab61d6bf102ee16fc9ff10d9ee55eeda
hassan <hassanak509@gmail.com>
2024-06-18 17:58:50 +0300

    used React-Toastify package

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,8 +1,20 @@
+import { toast } from "react-toastify";
+
 function Home() {
   return (
     <main>
-      <section className="container">
-        <h1>Home Page</h1>
+      <section className="container my-10">
+        <h1 className="text-4xl mb-3">Home Page</h1>
+        <button
+          className="btn my-3"
+          onClick={() => toast.success("Successful")}
+        >
+          click me to view a toast that you should return in success
+        </button>
+        <br />
+        <button className="btn my-3" onClick={() => toast.error("Error")}>
+          click me to view a toast that you should return in error
+        </button>
       </section>
     </main>
   );
